75ba0428137293a7943f4eea30622f055880e142
Matous Synek <xsynek3@gmail.com>
2024-11-24 16:27:27 +0100

    timer funfacts added

--- code/timer/js/timer.js
+++ code/timer/js/timer.js
@@ -1,6 +1,9 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Nov 25, 2024 20:25:00").getTime();
 
+const earth_speed = 29.7246; // km/s
+const hair_speed = 18.0; //nm/s
+
 // Update the count down every 1 second
 var x = setInterval(function() {
 
@@ -9,6 +12,8 @@ var x = setInterval(function() {
 
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
+
+  console.log(Math.round(distance/1000));
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -20,9 +25,12 @@ var x = setInterval(function() {
   document.getElementById("timer").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
+  document.getElementById("speed").innerHTML = "In that time the Earth travels cca " + Math.round(distance/1000 * earth_speed) + " km <br> and an average hair grows by cca " + Math.round(distance / 1000 * hair_speed) + " nm";
+
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "Done";
+    document.getElementById("speed").hidden = true;
   }
 }, 1000);
